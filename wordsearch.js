011ad924a514c4c11756af2969347ac4a9d56215
Bryn <bschulha@ualberta.ca>
2020-09-24 00:27:53 +0000

    Created tests for vertical words and an empty matrix

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
 const wordSearch = (letters, word) => { 
+    if (letters.length === 0) return false;
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    const lettersTransposed = transpose(letters);
+    const verticalJoin = lettersTransposed.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+
+    if (!l.includes(word)) return false
+    
 }
+
+const transpose = function(matrix) {
+    const output = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+      output.push([]);
+    }
+  
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j ++) {
+        output[j].push(matrix[i][j]);
+      }
+    }
+    return output;
+  };
 
 module.exports = wordSearch
